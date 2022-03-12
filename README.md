824f6fe77674d3048de8b56c11531f6056081c7f
Mr.Hope <zhangbowang1998@gmail.com>
2022-03-13 01:31:09 +0800

    chore: update deps

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# 个人博客
+# 个人博客 / Personal Blog
 
-[![作者: Mr.Hope](https://img.shields.io/badge/作者-Mr.Hope-blue.svg?style=for-the-badge)](https://mrhope.site)
+[![Author: Mr.Hope](https://img.shields.io/badge/Author-Mr.Hope-blue.svg?style=for-the-badge)](https://mrhope.site)
 [![License](https://img.shields.io/github/license/mister-hope/mister-hope.github.io?style=for-the-badge)](https://github.com/Mister-Hope/Mister-Hope.github.io/blob/master/LICENSE)
 
-使用 VuePress 与 [VuePress Theme Hope](https://vuepress-theme-hope.github.io/v2/zh/) 搭建的个人博客
+使用 [VuePress](https://v2.vuepress.vuejs.org/zh/) 与 [VuePress Theme Hope](https://vuepress-theme-hope.github.io/v2/zh/) 搭建的个人博客
 
-A personal blog using VuePress and [VuePress Theme Hope](https://vuepress-theme-hope.github.io/v2/)
+A personal blog using [VuePress](https://v2.vuepress.vuejs.org/) and [VuePress Theme Hope](https://vuepress-theme-hope.github.io/v2/)
 
 ## [博客地址](https://mrhope.site) | [Blog link](https://mister-hope.github.io)
--- src/.vuepress/themeConfig.ts
+++ src/.vuepress/themeConfig.ts
@@ -74,6 +74,49 @@ export default defineThemeConfig({
       appId: "LDBQGQC8Q9",
       apiKey: "5c3a7145aeba231c3b85b742d24fc24f",
       indexName: "mrhope",
+      locales: {
+        "/": {
+          placeholder: "搜索",
+          translations: {
+            button: {
+              buttonText: "搜索",
+              buttonAriaLabel: "搜索",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                openIssueText: "你认为该查询应该有结果？",
+                openIssueLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
     },
 
     feed: {
