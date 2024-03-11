249c495f25eea30907058c46b8d6388cc38f8158
Mister-Hope <mister-hope@outlook.com>
2024-03-11 05:53:26 +0000

    Deploying to gh-pages from @ Mister-Hope/Mister-Hope.github.io@175f1aac30c845a65c975a7c9fb3624e971f37cc 🚀

---
+++ assets/pageview-BSHL0osS.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as r}from"./app-BOB1QfUF.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://comment.mister-hope.com"};const i=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-BOB1QfUF.js").then(t=>t.M),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{i as updatePageview};
