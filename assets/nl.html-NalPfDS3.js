9bb3b6ced8dbb28440f214cb91c3d33435c86f51
Mister-Hope <mister-hope@outlook.com>
2024-04-06 18:36:02 +0000

    Deploying to gh-pages from @ Mister-Hope/Mister-Hope.github.io@8393b5dce15e5b437c4d5920ac377fff32dc8223 🚀

---
+++ assets/nl.html-NalPfDS3.js
@@ -0,0 +1,40 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as n,c as s,b as a}from"./app-BsmRcrkH.js";const t={},i=a(`<h1 id="nl" tabindex="-1"><a class="header-anchor" href="#nl"><span>nl</span></a></h1><p><code>nl</code> 命令用于显示行号。</p><p>下面是 <code>example.txt</code> 文件的内容。</p><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code>Lorem ipsum
+dolor sit amet,
+consetetur
+sadipscing elitr,
+<span class="token function">sed</span> diam nonumy
+eirmod tempor
+invidunt ut labore
+et dolore magna
+aliquyam erat, <span class="token function">sed</span>
+diam voluptua. At
+vero eos et
+accusam et justo
+duo dolores et ea
+rebum. Stet clita
+kasd gubergren,
+no sea takimata
+sanctus est Lorem
+ipsum dolor sit
+amet.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><code>nl</code> 命令让上面这段文本显示行号。</p><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code>$ <span class="token function">nl</span> -s<span class="token string">&quot;. &quot;</span> example.txt
+     <span class="token number">1</span>. Lorem ipsum
+     <span class="token number">2</span>. dolor sit amet,
+     <span class="token number">3</span>. consetetur
+     <span class="token number">4</span>. sadipscing elitr,
+     <span class="token number">5</span>. <span class="token function">sed</span> diam nonumy
+     <span class="token number">6</span>. eirmod tempor
+     <span class="token number">7</span>. invidunt ut labore
+     <span class="token number">8</span>. et dolore magna
+     <span class="token number">9</span>. aliquyam erat, <span class="token function">sed</span>
+    <span class="token number">10</span>. diam voluptua. At
+    <span class="token number">11</span>. vero eos et
+    <span class="token number">12</span>. accusam et justo
+    <span class="token number">13</span>. duo dolores et ea
+    <span class="token number">14</span>. rebum. Stet clita
+    <span class="token number">15</span>. kasd gubergren,
+    <span class="token number">16</span>. no sea takimata
+    <span class="token number">17</span>. sanctus est Lorem
+    <span class="token number">18</span>. ipsum dolor sit
+    <span class="token number">19</span>. amet.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><code>-s</code> 参数表示行号的后缀。</p>`,7),l=[i];function r(o,d){return n(),s("div",null,l)}const p=e(t,[["render",r],["__file","nl.html.vue"]]),u=JSON.parse('{"path":"/linux/bash/archives/commands/nl.html","title":"nl","lang":"zh-CN","frontmatter":{"description":"nl nl 命令用于显示行号。 下面是 example.txt 文件的内容。 nl 命令让上面这段文本显示行号。 -s 参数表示行号的后缀。","head":[["meta",{"property":"og:url","content":"https://mister-hope.com/linux/bash/archives/commands/nl.html"}],["meta",{"property":"og:site_name","content":"Mr.Hope"}],["meta",{"property":"og:title","content":"nl"}],["meta",{"property":"og:description","content":"nl nl 命令用于显示行号。 下面是 example.txt 文件的内容。 nl 命令让上面这段文本显示行号。 -s 参数表示行号的后缀。"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2021-01-27T06:58:54.000Z"}],["meta",{"property":"article:author","content":"Mr.Hope"}],["meta",{"property":"article:modified_time","content":"2021-01-27T06:58:54.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"nl\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2021-01-27T06:58:54.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Mr.Hope\\",\\"url\\":\\"https://mister-hope.com\\"}]}"],["link",{"rel":"alternate","type":"application/atom+xml","href":"https://mister-hope.com/atom.xml","title":"Mr.Hope Atom Feed"}],["link",{"rel":"alternate","type":"application/json","href":"https://mister-hope.com/feed.json","title":"Mr.Hope JSON Feed"}],["link",{"rel":"alternate","type":"application/rss+xml","href":"https://mister-hope.com/rss.xml","title":"Mr.Hope RSS Feed"}]],"date":"2020-06-03T17:08:34.000Z"},"headers":[],"git":{"createdTime":1591204114000,"updatedTime":1611730734000,"contributors":[{"name":"Mr.Hope","email":"zhangbowang1998@gmail.com","commits":2}]},"readingTime":{"minutes":0.56,"words":168},"filePathRelative":"linux/bash/archives/commands/nl.md","localizedDate":"2020年6月3日","excerpt":"","autoDesc":true}');export{p as comp,u as data};
