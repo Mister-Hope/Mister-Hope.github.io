6a83c4c4034428d1025f6c0d39530df8b0b5a3f6
Mister-Hope <mister-hope@outlook.com>
2024-04-21 11:07:52 +0000

    Deploying to gh-pages from @ Mister-Hope/Mister-Hope.github.io@2a028d6f4681b538352f5866ff1a4677aaf5786d 🚀

---
+++ assets/channel-MuI4gqNw.js
@@ -0,0 +1,1 @@
+import{aj as o,ak as n}from"./mermaid.core-DrGgL7F1.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/sort.html-BdH_fbdX.js
@@ -0,0 +1,24 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as a,b as o}from"./app-BBDjrnAY.js";const s={},r=o(`<h1 id="sort" tabindex="-1"><a class="header-anchor" href="#sort"><span>sort</span></a></h1><p><code>sort</code> 命令用于文本文件的排序。</p><p>下面是 <code>example.txt</code> 文件的内容。</p><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code>f
+b
+c
+g
+a
+e
+d
+</code></pre></div><p>执行 <code>sort</code> 命令对其进行排序。</p><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code>$ <span class="token function">sort</span> example.txt
+a
+b
+c
+d
+e
+f
+g
+</code></pre></div><h2 id="参数" tabindex="-1"><a class="header-anchor" href="#参数"><span>参数</span></a></h2><p><code>-R</code> 参数表示随机排序。</p><div class="language-bash" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token function">sort</span> <span class="token parameter variable">-R</span> example.txt
+b
+d
+a
+c
+g
+e
+f
+</code></pre></div>`,9),n=[r];function p(c,i){return t(),a("div",null,n)}const d=e(s,[["render",p],["__file","sort.html.vue"]]),h=JSON.parse('{"path":"/linux/bash/archives/commands/sort.html","title":"sort","lang":"zh-CN","frontmatter":{"date":"2020-06-04T00:00:00.000Z","description":"sort sort 命令用于文本文件的排序。 下面是 example.txt 文件的内容。 执行 sort 命令对其进行排序。 参数 -R 参数表示随机排序。","head":[["meta",{"property":"og:url","content":"https://mister-hope.com/linux/bash/archives/commands/sort.html"}],["meta",{"property":"og:site_name","content":"Mr.Hope"}],["meta",{"property":"og:title","content":"sort"}],["meta",{"property":"og:description","content":"sort sort 命令用于文本文件的排序。 下面是 example.txt 文件的内容。 执行 sort 命令对其进行排序。 参数 -R 参数表示随机排序。"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-07T05:21:25.000Z"}],["meta",{"property":"article:author","content":"Mr.Hope"}],["meta",{"property":"article:published_time","content":"2020-06-04T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-04-07T05:21:25.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"sort\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2020-06-04T00:00:00.000Z\\",\\"dateModified\\":\\"2024-04-07T05:21:25.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Mr.Hope\\",\\"url\\":\\"https://mister-hope.com\\"}]}"],["link",{"rel":"alternate","type":"application/atom+xml","href":"https://mister-hope.com/atom.xml","title":"Mr.Hope Atom Feed"}],["link",{"rel":"alternate","type":"application/json","href":"https://mister-hope.com/feed.json","title":"Mr.Hope JSON Feed"}],["link",{"rel":"alternate","type":"application/rss+xml","href":"https://mister-hope.com/rss.xml","title":"Mr.Hope RSS Feed"}]]},"headers":[{"level":2,"title":"参数","slug":"参数","link":"#参数","children":[]}],"git":{"createdTime":1591204114000,"updatedTime":1712467285000,"contributors":[{"name":"Mr.Hope","email":"zhangbowang1998@gmail.com","commits":2},{"name":"Mr.Hope","email":"mister-hope@outlook.com","commits":1}]},"readingTime":{"minutes":0.26,"words":77},"filePathRelative":"linux/bash/archives/commands/sort.md","localizedDate":"2020年6月4日","excerpt":"","autoDesc":true}');export{d as comp,h as data};
