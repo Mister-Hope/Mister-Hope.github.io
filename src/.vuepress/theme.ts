2414f85b5459f70c28ef5661d1443c861c584499
Mister-Hope <mister-hope@outlook.com>
2025-01-14 02:18:14 +0800

    build: bump deps

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -14,8 +14,6 @@ export default hopeTheme(
 
     favicon: "/favicon.ico",
 
-    iconAssets: "//at.alicdn.com/t/font_2410206_vuzkjonf4s9.css",
-
     logo: "/logo.svg",
 
     repo: "Mister-Hope/Mister-Hope.github.io",
@@ -127,17 +125,16 @@ export default hopeTheme(
         rss: true,
       },
 
+      icon: {
+        assets: "//at.alicdn.com/t/font_2410206_vuzkjonf4s9.css",
+      },
+
       pwa: {
         themeColor: "#5c92d1",
         cacheHTML: false,
         maxSize: 3072,
         apple: {
           icon: "/assets/icon/apple-touch-icon.png",
-          statusBarColor: "white",
-        },
-        msTile: {
-          image: "/assets/icon/ms-icon-144.png",
-          color: "#ffffff",
         },
         manifest: {
           name: "Mr.Hope 的个人博客",
