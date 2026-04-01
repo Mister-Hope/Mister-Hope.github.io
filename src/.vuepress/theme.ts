480e9b944eb0e64f993e0f2c75473c54ce3a01d2
Mr.Hope <mister-hope@outlook.com>
2026-04-01 11:24:25 +0800

    chore: tweaks

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -55,8 +55,7 @@ export default hopeTheme(
         navbar: enNavbarConfig,
         sidebar: enSidebarConfig,
 
-        footer:
-          'Theme by <a href="https://theme-hope.vuejs.press">vuepress-theme-hope</a>',
+        footer: 'Theme by <a href="https://theme-hope.vuejs.press">vuepress-theme-hope</a>',
 
         copyright: "MIT Licensed, © 2019-present Mr.Hope",
 
@@ -131,103 +130,6 @@ export default hopeTheme(
 
       icon: {
         assets: "//at.alicdn.com/t/font_2410206_vuzkjonf4s9.css",
-      },
-
-      pwa: {
-        themeColor: "#5c92d1",
-        cacheHTML: false,
-        maxSize: 3072,
-        apple: {
-          icon: "/assets/icon/apple-touch-icon.png",
-        },
-        manifest: {
-          name: "Mr.Hope 的个人博客",
-          short_name: "Mr.Hope Blog",
-          description: "Mr.Hope 的个人博客",
-          theme_color: "#5c92d1",
-          icons: [
-            {
-              src: "/assets/icon/chrome-192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-512.png",
-              sizes: "512x512",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-192.png",
-              sizes: "192x192",
-              purpose: "maskable",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-512.png",
-              sizes: "512x512",
-              purpose: "maskable",
-              type: "image/png",
-            },
-          ],
-          shortcuts: [
-            {
-              name: "分类",
-              short_name: "分类",
-              icons: [
-                {
-                  src: "/assets/icon/category-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/category/",
-              description: "文章分类分组",
-            },
-            {
-              name: "标签",
-              short_name: "标签",
-              icons: [
-                {
-                  src: "/assets/icon/tag-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/tag/",
-              description: "文章标签分组",
-            },
-            {
-              name: "时间线",
-              short_name: "时间线",
-              icons: [
-                {
-                  src: "/assets/icon/timeline-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/timeline/",
-              description: "时间线文章列表",
-            },
-            {
-              name: "个人介绍",
-              short_name: "个人介绍",
-              icons: [
-                {
-                  src: "/assets/icon/about-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/about/",
-              description: "个人介绍",
-            },
-          ],
-        },
       },
     },
   },
